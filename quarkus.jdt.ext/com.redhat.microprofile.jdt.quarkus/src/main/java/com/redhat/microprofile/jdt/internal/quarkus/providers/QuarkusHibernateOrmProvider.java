105c46d52f322a0bc231443602591f83498fdd7a
azerr <azerr@redhat.com>
2020-11-16 20:15:48 +0100

    `quarkus.hibernate-orm.database.generation` values should be enumerated
    Fixes #317
    
    Signed-off-by: azerr <azerr@redhat.com>

---
+++ quarkus.jdt.ext/com.redhat.microprofile.jdt.quarkus/src/main/java/com/redhat/microprofile/jdt/internal/quarkus/providers/QuarkusHibernateOrmProvider.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+* Copyright (c) 2020 Red Hat Inc. and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v. 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+* which is available at https://www.apache.org/licenses/LICENSE-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+*
+* Contributors:
+*     Red Hat Inc. - initial API and implementation
+*******************************************************************************/
+package com.redhat.microprofile.jdt.internal.quarkus.providers;
+
+/**
+ * Properties provider that provides static Quarkus properties
+ * 
+ * @author Angelo ZERR
+ * 
+ * @see https://github.com/eclipse/microprofile-health/blob/master/spec/src/main/asciidoc/protocol-wireformat.adoc
+ *
+ */
+public class QuarkusHibernateOrmProvider extends AbstractStaticQuarkusPropertiesProvider {
+
+	public QuarkusHibernateOrmProvider() {
+		super("/static-properties/quarkus-hibernate-orm-metadata.json");
+	}
+}
